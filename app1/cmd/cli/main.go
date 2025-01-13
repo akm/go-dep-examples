112c1e581314eafecbfb6127d3d24b934d557ccb
akm <akm2000@gmail.com>
2025-01-13 11:40:14 +0900

    ✨ Use app1/add_string.go from app1/cmd/cli/main.go

--- app1/cmd/cli/main.go
+++ app1/cmd/cli/main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"app1"
 	"fmt"
-
-	"github.com/akm/go-dep-examples/lib1"
+	"os"
 )
 
 func main() {
-	fmt.Printf("%d + %d = %d\n", 1, 2, lib1.Add(1, 2))
+	v1 := os.Args[1]
+	v2 := os.Args[2]
+	res, err := app1.AddString(v1, v2)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println(res)
+}
+
+func init() {
+	println("app1/cmd/cli/main.go init")
 }
